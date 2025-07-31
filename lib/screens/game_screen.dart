49b1a6c1ec435658cbb26ba82b408782eb89dce8
Xpell <lcsouzah@gmail.com>
2025-07-31 19:01:50 -0400

    refactor: delegate pause and logic control to SafeAreaScreen

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -2,12 +2,12 @@
 
 
 import 'package:flutter/material.dart';
-import '../widget/tap_feedback_overlay.dart'; //
+import 'package:provider/provider.dart';
+import '../widget/tap_feedback_overlay.dart'; // need improvement
 import '../models/alphabet_game.dart';
 import '../widget/tile.dart';
 import '../utils/sound_manager.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../screens/safe_area.dart';
+import '../utils/pause_manager.dart';
 
 
 class GameScreen extends StatefulWidget {
@@ -16,33 +16,106 @@ class GameScreen extends StatefulWidget {
   final List<String> dictionary;
   final ScoringOption scoringOption;
   final VoidCallback onPauseToggle;
+  final void Function() onRewardedAdRequest;
+  final int maxHints;
+  final int adUsesThisMatch;
+  final int maxAdUsesPerMatch;
 
-  const GameScreen({super.key,
+
+  const GameScreen({
+    super.key,
     required this.game,
     required this.dictionary,
     required this.onCorrectWord,
     required this.scoringOption,
     required this.onPauseToggle,
+    required this.onRewardedAdRequest,
+    required this.maxHints,
+    required this.adUsesThisMatch,
+    required this.maxAdUsesPerMatch,
+
   });
 
   @override
 
-  _GameScreenState createState() => _GameScreenState();
+  GameScreenState createState() => GameScreenState();
 }
 
-class _GameScreenState extends State<GameScreen> {
+class GameScreenState extends State<GameScreen> with TickerProviderStateMixin {
 
-  bool _showTutorial = false;
 
-  int _hintsUsed = 0;
-  final int _maxHints = 3;
+  late AnimationController _hintButtonController;
+  late Animation<double> _hintButtonAnimation;
 
+
+
+  late int _hintsUsed ;
+  late int _maxHints;
 
   int moveCounter = 0;// Add this line to initialize the move counter
   List<int> _highlightedIndices = [];
+  final List<int> _disappearingIndices = [];
 
 
+  @override
 
+  void initState() {
+    super.initState();
+    _maxHints =  widget.maxHints;
+    _hintsUsed = 0;
+
+    _hintButtonController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..repeat(reverse: true);
+
+    _hintButtonAnimation = Tween<double>(begin: 1.0, end: 1.1)
+        .animate(CurvedAnimation(parent: _hintButtonController, curve: Curves.easeInOut));
+  }
+
+
+  @override
+  void dispose() {
+    _hintButtonController.dispose();
+    super.dispose();
+  }
+
+
+  void addHints(int amount) {
+    setState(() {
+      _maxHints += amount;
+
+      // If user had used all hints, reset hintsUsed to allow usage again
+      if (_hintsUsed >= _maxHints) {
+        _hintsUsed = _maxHints - amount;
+      }
+
+      // Ensure hintsUsed is never negative
+      _hintsUsed = _hintsUsed.clamp(0, _maxHints);
+      debugPrint('🧠 addHints called: maxHints=$_maxHints | hintsUsed=$_hintsUsed');
+    });
+  }
+
+  void _handleTileTap(int index) {
+    final pauseManager = Provider.of<PauseManager>(context, listen: false);
+
+    // block movement if paused
+    if (pauseManager.isPaused && pauseManager.pauseReason == PauseReason.manual) {
+      return;
+    }
+
+    //auto resume if not manual pause
+    if (pauseManager.isPaused && pauseManager.pauseReason != PauseReason.manual) {
+      pauseManager.forceResume();
+    }
+
+    if(widget.game.moveTile(index)){
+      SoundManager.playSound('tileMove');
+      moveCounter++; // Increment the move counter
+      setState(() {});
+    }
+    _checkWord();
+  }
 
   String? _findClosestWord(List<String> boardLetters) {
     int bestScore = 0;
@@ -67,48 +140,6 @@ class _GameScreenState extends State<GameScreen> {
     return bestMatch;
   }
 
-
-  void _showHint() async {
-
-    if(_hintsUsed >= _maxHints) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("You have used all your hits.")),
-      );
-      return;
-
-    }
-
-    List<String> boardLetters = [];
-    for (var letter in widget.game.letters) {
-      if (letter != ' ') boardLetters.add(letter);
-    }
-
-    String? hintWord = _findClosestWord(boardLetters);
-
-    if (hintWord != null) {
-      _hintsUsed++;
-
-      final indices = _findMatchingIndices(hintWord);
-
-      setState(() {
-        _highlightedIndices = indices;
-      });
-
-      await Future.delayed(const Duration(milliseconds: 800));
-
-      setState(() {
-        _highlightedIndices.clear();
-      });
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("No obvious hints available right now.")),
-      );
-    }
-  }
-
-
-  //late SoundManager soundManager = SoundManager();
-
   List<int> _findMatchingIndices(String word) {
     List<int> indices = [];
     final tiles = widget.game.letters;
@@ -125,16 +156,46 @@ class _GameScreenState extends State<GameScreen> {
     return indices;
   }
 
+  void _showHint() async {
+   // final pauseManager = Provider.of<PauseManager>(context, listen: false);
 
+    debugPrint('💡 Requesting hint → hintsUsed: $_hintsUsed | maxHints: $_maxHints');
 
-  void _handleTileTap(int index) {
-    if(widget.game.moveTile(index)){
-       SoundManager.playSound('tileMove');
-       moveCounter++; // Increment the move counter
-       setState(() {});
+    if (_hintsUsed >= _maxHints) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("You have used all your hints.")),
+      );
+      return;
     }
-    _checkWord();
-      setState(() {});
+
+    //pauseManager.pause(PauseReason.hint);
+
+    List<String> boardLetters = [];
+    for (var letter in widget.game.letters) {
+      if (letter != ' ') boardLetters.add(letter);
+    }
+
+    String? hintWord = _findClosestWord(boardLetters);
+
+    if (hintWord != null) {
+      _hintsUsed++;
+      final indices = _findMatchingIndices(hintWord);
+
+      setState(() {
+        _highlightedIndices = indices;
+      });
+
+      await Future.delayed(const Duration(milliseconds: 800));
+
+      setState(() {
+        _highlightedIndices.clear();
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("No obvious hints available right now.")),
+      );
+    }
+    //pauseManager.resume(PauseReason.hint);
   }
 
   void _checkWord() async {
@@ -143,15 +204,15 @@ class _GameScreenState extends State<GameScreen> {
 
     // Check based on the selected scoring option
     switch (widget.scoringOption) {
-      case ScoringOption.Horizontal:
+      case ScoringOption.horizontal:
       // Assuming getWord() returns a list of horizontally formed words
         formedWords.add(widget.game.getWord()); // Adjusted to hypothetical method
         break;
-      case ScoringOption.Vertical:
+      case ScoringOption.vertical:
       // For vertical, we're already clear on getWordVertical()
         formedWords.add(widget.game.getWordVertical()); // Assuming it returns a single vertical word
         break;
-      case ScoringOption.Both:
+      case ScoringOption.both:
       // Combine both horizontal and vertical checks
         formedWords.add(widget.game.getWord()); // Adjusted to hypothetical method
         formedWords.add(widget.game.getWordVertical());
@@ -162,19 +223,45 @@ class _GameScreenState extends State<GameScreen> {
     for (String word in formedWords) {
       if (widget.dictionary.contains(word)) {
         widget.onCorrectWord(word);
-        print('🎉 Matched word: $word');
+        debugPrint('🎉 Matched word: $word');
 
-        // Highlight tiles that form the word
-        setState(() {
-          _highlightedIndices = _findMatchingIndices(word);
-        });
+        final indices = _findMatchingIndices(word);
+
+        // ✅ Sequentially highlight each tile
+        for(int i = 0; i < indices.length; i++){
+          await Future.delayed(Duration(milliseconds: 120 * i),() {
+            if(!mounted) return;
+            setState(() {
+              _highlightedIndices.add(indices[i]);
+            });
+          });
+        }
+
+        // ✅ Keep them green for a moment
+
+        await Future.delayed(const Duration(milliseconds: 50));
+
+        // ✅ Sequentially "disappear" (shrink them one by one)
+        for (int i = 0; i < indices.length; i++) {
+          await Future.delayed(Duration(milliseconds: 50 * i), () {
+            if (!mounted) return;
+            setState(() {
+
+              //replace with disappearing animation
+              _highlightedIndices.remove(indices[i]);
+
+            });
+          });
+        }
+
+        // ✅ After all done → clear word & generate new letters
 
         // Wait before clearing
-        await Future.delayed(const Duration(milliseconds: 600));
+        await Future.delayed(const Duration(milliseconds: 50));
 
 
         setState(() {
-          _highlightedIndices.clear();
+          _disappearingIndices.clear();
           widget.game.clearWord();
           widget.game.generateNewLetters();
         });
@@ -184,191 +271,132 @@ class _GameScreenState extends State<GameScreen> {
     }
 
     // If no word is formed correctly, optionally handle this scenario
-    print('The formed word is not correct.$formedWords' );
+    debugPrint('The formed word is not correct.$formedWords' );
     setState(() {}); // Ensure to refresh the UI if needed
   }
 
 
-
-
   @override
-
-  void initState() {
-    super.initState();
-    _loadTutorialStatus();
-}
-
-Future<void> _loadTutorialStatus() async {
-    final prefs = await SharedPreferences.getInstance();
-    final seenTutorial = prefs.getBool('seen_tutorial') ?? false;
-
-    if(!seenTutorial){
-      setState(() {
-        _showTutorial = true;
-      });
-      prefs.setBool('seen_tutorial', true);
-    }
-}
-
-void _showPauseMenu(){
-    showDialog(
-    context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('Pause'),
-            content: const Text('What would you like to do?'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context); //Resume
-                },
-                child: const Text('Resume'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-              setState(() {
-                _showTutorial = true; //replay tutorial
-              });
-            },
-            child: const Text('Replay Tutorial'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-              Navigator.pop(context); //Exit game
-            },
-            child: const Text('Exit Game'),
-          )]
-
-          );
-        }
-
-    );
-}
-
   Widget build(BuildContext context) {
+    final pauseManager = Provider.of<PauseManager>(context);
+
+    debugPrint('💡 BUILD → hintsUsed=$_hintsUsed | maxHints=$_maxHints');
+    debugPrint('📺 Ads → adUsesThisMatch=${widget.adUsesThisMatch} | maxAdUsesPerMatch=${widget.maxAdUsesPerMatch}');
+
+    // Decide button label & icon dynamically
+    String label ;
+    Icon icon;
+
+    final canUseHint =  _hintsUsed < _maxHints;
+    final canUseAd = widget.adUsesThisMatch < widget.maxAdUsesPerMatch;
+
+    if (canUseHint) {
+      label = 'Hint (${_maxHints - _hintsUsed})';
+      icon = const Icon(Icons.lightbulb_outline);
+    } else if (canUseAd) {
+      label = 'Get +3 Hints';
+      icon = const Icon(Icons.video_library);
+    } else {
+      label = 'No more hints';
+      icon = const Icon(Icons.block);
+    }
+
     return TouchFeedbackOverlay(
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton.extended(
-            onPressed: _showHint,
-            label: Text('Hint (${_maxHints - _hintsUsed})'),
-            icon: const Icon(Icons.lightbulb_outline),
-            backgroundColor: _hintsUsed < _maxHints ? Colors.amber : Colors.grey,
-        ),
-
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-
-
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Moves: $moveCounter'),
-        actions:[
-          IconButton(
-            icon: const Icon(Icons.pause),
-            onPressed: () {
-                widget.onPauseToggle();
-                _showPauseMenu();
-                },
-          ),// Display move counter here
-        ],
-      ),
-
-
-              body: Stack(
+        child: Scaffold(
+            floatingActionButton: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-              //Game board
-              GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              ),
-              itemCount: 16,
-              itemBuilder: (context, index) {
-              final letter = widget.game.letters[index];
-              return TileWidget(
-              letter: letter,
-              onTap: () => _handleTileTap(index),
-              highlighted: _highlightedIndices.contains(index),
-              );
-              },
-              ),
-              //Tutorial Overlay
-              if(_showTutorial)
-              Positioned.fill(
-              child: Container(
-              color: Colors.black.withOpacity(0.75),
-              child: Stack(
-              children: [
-              //Fading hand Animation
-              Positioned(
-              left: MediaQuery.of
-                (context).size.width * 0.25,
-                top: MediaQuery.of(context).size.height * 0.65,
-                child: TweenAnimationBuilder<double>(
-                  tween: Tween(begin: 0, end: 1),
-                  duration: const Duration(seconds: 1),
-                  curve: Curves.easeInOut,
-                  builder: (context, value, child) {
-                    return Opacity(
-                      opacity : 1 - value,
-                      child: Transform.translate(
-                        offset: Offset(value * 40, -value * 40),
-                        child: child,
-                      ),
-                    );
-                  },
-                  child: Image.asset(
-                    'assets/images/hand_pointer.gif',
-                    width: 64,
-                    height: 64,
-
+                ScaleTransition(
+                  scale: canUseHint
+                      ? _hintButtonAnimation
+                      : const AlwaysStoppedAnimation(1.0),
+                  child: FloatingActionButton.extended(
+                    onPressed: (!canUseHint && !canUseAd)
+                        ? null
+                        : () {
+                      if (canUseHint) {
+                        _showHint();
+                      } else {
+                        widget.onRewardedAdRequest();
+                        setState(() {});
+                      }
+                    },
+                    label: Text(label),
+                    icon: icon,
+                    backgroundColor:
+                    _hintsUsed < _maxHints ? Colors.amber : Colors.grey,
                   ),
-                  onEnd: () {
-                    if(_showTutorial) setState((){}); // loop animation
-                  },
+
                 ),
-              ),
+              ],
+            ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
 
 
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text('Moves: $moveCounter'),
+            ),
 
-        // Text + button column
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "👆 Tap and slide tiles",
-                style: TextStyle(color: Colors.white, fontSize: 24),
-                ),
-              const SizedBox(height: 20),
-              const Text(
-                "🧠 Form real English words",
-                style: TextStyle(color: Colors.white60, fontSize: 18),
-                ),
-              const SizedBox(height: 20),
-              const Text(
-                '💡 Use the hint button when stuck',
-                style: TextStyle(color: Colors.yellowAccent, fontSize: 18),
-                ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                onPressed:() {
-                  setState((){
-                    _showTutorial = false;
-                });
-                },
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
-                child: const Text ("Got it, LET'S GO!"),
-              )
-            ],
-          ),
+
+            body: Stack(
+                children: [
+                  //Game board
+                  GridView.builder(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                    ),
+                    itemCount: 16,
+                    itemBuilder: (context, index) {
+                      final letter = widget.game.letters[index];
+
+
+                      return IgnorePointer(
+                        ignoring: pauseManager.isPaused,
+                        child: AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 200),
+                        transitionBuilder: (child, animation) {
+                          return ScaleTransition(scale: animation, child: child);
+                        },
+                        child: TileWidget(
+                          key:ValueKey(letter + index.toString()),
+                          letter: letter,
+                          onTap: () => _handleTileTap(index),
+                          highlighted: _highlightedIndices.contains(index), // stays green glow
+                          disappearing: _disappearingIndices.contains(index), // wont shrink on hint
+                        ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+            ),
         ),
-      ],
-          ),
-    ),
-    ),
-    ]
-    ))
     );
   }
 }
+
+
+
+
+
+
+  //late SoundManager soundManager = SoundManager();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
