1372d0144518c3876ba461814b3219b62ba460a2
Xpell <lcsouzah@gmail.com>
2025-08-22 17:58:32 -0400

    Gradle + pubspec updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,17 +20,17 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
         applicationId = "com.word_game_app"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 35
         versionCode = 13 // ← ✅ Add this! Must be incremented every time to upload new version to Play Store
         versionName = "2.0.0"
