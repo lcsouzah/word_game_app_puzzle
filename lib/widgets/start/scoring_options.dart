8142369dcd0356bcb25b3a183bad4f6080bcb6f6
Xpell <lcsouzah@gmail.com>
2025-09-14 17:28:19 -0400

    Replace Radio widgets with RadioGroup

--- lib/widgets/start/scoring_options.dart
+++ lib/widgets/start/scoring_options.dart
@@ -11,25 +11,52 @@ class ScoringOptions extends StatelessWidget {
     required this.onChanged,
   });
 
-  Widget _buildOption(ScoringOption option) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Radio<ScoringOption>(
-          value: option,
-          groupValue: groupValue,
-          onChanged: onChanged,
-        ),
-        Text(option.name),
-      ],
+  @override
+  Widget build(BuildContext context) {
+    return RadioGroup<ScoringOption>(
+      options: ScoringOption.values,
+      groupValue: groupValue,
+      onChanged: onChanged,
+      labelBuilder: (option) => Text(option.name),
     );
   }
+}
+
+typedef RadioLabelBuilder<T> = Widget Function(T value);
+
+class RadioGroup<T> extends StatelessWidget {
+  final List<T> options;
+  final T groupValue;
+  final ValueChanged<T?> onChanged;
+  final RadioLabelBuilder<T> labelBuilder;
+
+  const RadioGroup({
+    super.key,
+    required this.options,
+    required this.groupValue,
+    required this.onChanged,
+    required this.labelBuilder,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: ScoringOption.values.map(_buildOption).toList(),
+      children: options
+          .map(
+            (option) => Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Radio<T>(
+              value: option,
+              groupValue: groupValue,
+              onChanged: onChanged,
+            ),
+            labelBuilder(option),
+          ],
+        ),
+      )
+          .toList(),
     );
   }
 }
